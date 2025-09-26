00ae62407d5cc5486047e645043c8b018139771a
Bryan-Nelson-2 <bryan.nelson@stu.ocsb.ca>
2025-09-26 13:26:40 -0400

    Program turns an LED on and off when A and B are pressed

--- main.ts
+++ main.ts
@@ -1,8 +1,22 @@
-/* Copyright (c) 2020 MTHS All rights reserved
- *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+/* Copyright (c) 2025 MTHS All rights reserved
+*
+* Created by: Alex
+* Created on: Sep 2025
+* This program turns an LED on and off with buttons A and B.
 */
 
-basic.showString('Hello, World!')
+// clears screen then shows a happy face
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+
+// turns on LED with B button
+input.onButtonPressed(Button.A, function () {
+    basic.showIcon(IconNames.Yes)
+    pins.digitalWritePin(DigitalPin.P16, 1)
+})
+
+// turns of LED with A button
+input.onButtonPressed(Button.B, function () {
+    basic.showIcon(IconNames.No)
+    pins.digitalWritePin(DigitalPin.P16, 0)
+})
